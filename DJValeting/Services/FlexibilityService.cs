d319991aa1b688381c825ceac7fba0d975e68ed7
lhowie8 <luke.howie.8@googlemail.com>
2023-05-15 20:17:20 +0100

    Added flexibility and vehicle size services

---
+++ DJValeting/Services/FlexibilityService.cs
@@ -0,0 +1,20 @@
+﻿using DJValeting.Data;
+using DJValeting.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DJValeting.Services
+{
+    public class FlexibilityService : IFlexibilityService
+    {
+        private readonly BusinessDbContext _context;
+
+        public FlexibilityService(BusinessDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Flexibility>> GetFlexibilityList()
+        {
+            return await _context.Flexibilities.OrderBy(x => x.DisplayOrder).ToListAsync();
+        }
+    }
+}
---
+++ DJValeting/Services/VehicleSizeService.cs
@@ -0,0 +1,19 @@
+﻿using DJValeting.Data;
+using DJValeting.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DJValeting.Services
+{
+    public class VehicleSizeService : IVehicleSizeService
+    {
+        private readonly BusinessDbContext _context;
+        public VehicleSizeService(BusinessDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<VehicleSize>> GetVehicleSizeList()
+        {
+            return await _context.VehicleSizes.OrderBy(x => x.DisplayOrder).ToListAsync();
+        }
+    }
+}
